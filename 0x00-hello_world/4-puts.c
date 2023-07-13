7502367c3443e4fc434419bf17c9c3c0a4724ab7
naol lamesa <natilamesa19@gmail.com>
2023-07-13 22:03:25 +0300

    naol

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
-
-int main() {
-    /*
-     * hello_world
-    */
+/*
+ * hello_world
+*/
+int main() 
+{  
     puts("Programming is like building a multilingual puzzle");
-
     return 0;
 }
