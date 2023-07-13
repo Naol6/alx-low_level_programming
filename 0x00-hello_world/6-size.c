31b6fac02389a0bf19ee869df0e29869f820fcf8
naol lamesa <natilamesa19@gmail.com>
2023-07-13 23:21:06 +0300

    naol

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,16 +7,15 @@
  */
 int main(void)
 {
-	int z;
-	long int x;
-	long long int y;
-	char v;
-	float a;
-    
-    printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(v));
-	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(z));
-	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(x));
-	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(y));
-	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(a));
-	return (0);
+int z;
+long int x;
+long long int y;
+char v;
+float a;
+printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(v));
+printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(z));
+printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(x));
+printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(y));
+printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(a));
+return (0);
 }
