e1e887e5d29b262b13494b7900d22ff89a9afbaf
naol lamesa <natilamesa19@gmail.com>
2023-07-18 15:45:12 +0300

    hy me

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the lowercase alphabet 10 times, followed by a new line
+ * Return: Always 0
+*/
+void print_alphabet_x10(void)
+{
+int i, j;
+for (i = 0; i < 10; i++)
+{
+for (j = 97; j <= 122; j++)
+{
+_putchar(j);
+}
+}
+_putchar('\n');
+}
