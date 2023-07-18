dbc8dc0e2e4b1ba5f2c50c0b845dbfc6d45893c2
naol lamesa <natilamesa19@gmail.com>
2023-07-19 01:35:50 +0300

    habasha

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+void print_times_table(int n)
+{
+if (n < 0 || n > 15)
+{
+return;
+}
+for (int i = 0; i <= n; i++)
+{
+for (int j = 0; j <= n; j++)
+{
+int result = i * j;
+printf("%d", result);
+if (j != n)
+{
+printf(",   ");
+}
+}
+printf("\n");
+}
+}
