4fc348e969c776aea5af58b7cdebd3373b22c1bb
naol lamesa <natilamesa19@gmail.com>
2023-07-18 20:00:23 +0300

    snap

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,7 +8,12 @@
 
 int print_last_digit(int n)
 {
-int lastDigit = n % 10;
-printf("The last digit is %d\n", lastDigit);
-return (lastDigit);
+int x;
+if (n < 0)
+n = -n;
+x = n % 10;
+if (x < 0)
+x = -x;
+_putchar(x + '0');
+return (x);
 }
