1bcf964765626c65149800f589d5d000fb89b91d
naol lamesa <natilamesa19@gmail.com>
2023-07-18 23:08:16 +0300

    for ever

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,11 +1,18 @@
 #include <stdio.h>
+/**
+ * jack_bauer - for loop
+ * Return: print the 00:00 to 23:59
+*/
 
-void jack_bauer(void) {
-    int hour, minute;
+void jack_bauer(void)
+{
+int hour, minute;
 
-    for (hour = 0; hour < 24; hour++) {
-        for (minute = 0; minute < 60; minute++) {
-            printf("%02d:%02d\n", hour, minute);
-        }
-    }
+for (hour = 0; hour < 24; hour++)
+{
+for (minute = 0; minute < 60; minute++)
+{
+printf("%02d:%02d\n", hour, minute);
+}
+}
 }
