757c8c6947fea597741e2916a54f9f47d63cdb9b
naol lamesa <natilamesa19@gmail.com>
2023-07-20 23:15:40 +0300

    more

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isdigit - chacks for digit
+ * @c: it's value
+ * Return: return both as asked
+ */
+int _isdigit(int c)
+{
+if (c >= 48 && c <= 57)
+{
+return (1);
+}
+return (0);
+}
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * mul - multiplication of two number
+ * @a: type
+ * @b: type
+ * Return: return multi or the value
+ */
+int mul(int x, int y)
+{
+long int multi;
+multi = x * y;
+return (multi);
+}
