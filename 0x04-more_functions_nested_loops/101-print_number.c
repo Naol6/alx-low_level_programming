bef5526079e137eb48122266015420814ece0c97
naol lamesa <natilamesa19@gmail.com>
2023-07-21 02:19:56 +0300

    lavel up

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -8,7 +8,6 @@
 void print_number(int n)
 {
 unsigned int num;
-
 if (n < 0)
 {
 num = -n;
@@ -18,11 +17,9 @@ else
 {
 num = n;
 }
-
 if (num / 10 != 0)
 {
 print_number(num / 10);
 }
-
 _putchar((num % 10) + '0');
 }
