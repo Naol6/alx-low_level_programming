f521dbffb7c19c475a8ad1eea461993904d24b55
naol lamesa <natilamesa19@gmail.com>
2023-07-27 00:17:56 +0300

    magic

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_number - print int
+ * @n: result must be int
+ */
+void print_number(int n)
+{
+unsigned int n1;
+if (n < 0)
+{
+n1 = -n;
+_putchar('-');
+}
+else
+{
+n1 = n;
+}
+if (n1 / 10)
+print_number(n1 / 10);
+_putchar((n1 % 10) + '0');
+}
