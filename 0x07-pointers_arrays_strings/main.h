90dc1ae10f92f7f4cc755bbe79247a3b81fa3444
naol lamesa <natilamesa19@gmail.com>
2023-07-31 23:11:40 +0300

    anamabasiba

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -5,5 +5,6 @@ char *_memset(char *x, char y, unsigned int z);
 int _putchar(char c);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *aster, char tedy);
+unsigned int _strspn(char *s, char *accept);
 
 #endif
