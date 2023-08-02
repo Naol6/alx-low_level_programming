036c1bb81b51b1a9e8a33bcccc6f7c372998b142
naol lamesa <natilamesa19@gmail.com>
2023-08-02 16:31:08 +0300

    transaction

--- chack.c
+++ chack.c
@@ -1,27 +1,17 @@
 #include <stdio.h>
 
-/**
- * rot13 - encodes a string into rot13
- * @s: string to encode
- *
- * Return: address of s
- */
-char *rot13(char *s)
+void print(int nb)
 {
-	int i, j;
-	char a[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	char b[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+    printf("%d", nb);
+    -- nb;
+    if (nb > 0) 
+    {
+        print(nb);
+    }
+}
 
-	for (i = 0; *(s + i); i++)
-	{
-		for (j = 0; j < 52; j++)
-		{
-			if (a[j] == *(s + i))
-			{
-				*(s + i) = b[j];
-				break;
-			}
-		}
-	}
-	return (s);
+int main(void)
+{
+    print(4);
+    return (0);
 }
