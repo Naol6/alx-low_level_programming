b0d4e0e089f693ccdb3637f35e4446a72c89e56f
naol lamesa <natilamesa19@gmail.com>
2023-08-03 01:54:11 +0300

    part

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -11,5 +11,8 @@ int _sqrt_recursion(int n);
 int power_operation(int n, int c);
 int is_prime(unsigned int n, unsigned int c);
 int is_prime_number(int n);
+int _strlen_recursion(char *s);
+int comparator(char *s, int n1, int n2);
+int is_palindrome(char *s);
 
 #endif
