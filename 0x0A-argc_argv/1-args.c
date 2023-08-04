d6c0cf14b917f3f22eaf617b3a74dbd5c154fcfd
naol lamesa <natilamesa19@gmail.com>
2023-08-05 01:05:39 +0300

    vuv

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - prints all arguments it receives.
+ * main - prints the number of arguments passed into it.
  * @argc: argument count
  * @argv: arguments
  *
@@ -9,9 +9,7 @@
  */
 int main(int argc, char **argv)
 {
-int i;
-for (i = 0; i < argc; i++)
-printf("%s\n", argv[i]);
-
+(void)argv;
+printf("%d\n", argc - 1);
 return (0);
 }
