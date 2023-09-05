69e62a1462977cfd6c21a5bcb1a05c9ff21cf5ae
naol lamesa <natilamesa19@gmail.com>
2023-09-05 16:01:03 +0300

    commit

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,86 +1,96 @@
-#include "holberton.h"
+#include "main.h"
 
-#define MAXSIZE 1024
-
+char *create_str(char *file);
+void close_file(int num);
 
 /**
- * __exit - prints error messages and exits with exit number
- *
- * @error: either the exit number or file descriptor
- * @str: name of either file_in or file_out
- * @fd: file descriptor
- *
- * Return: 0 on success
-*/
-int __exit(int error, char *str, int fd)
+ * create_str - Create a str object
+ * @file: file name
+ * Return: char*
+ */
+
+char *create_str(char *file)
 {
-switch (error)
-{
-case 97:
-dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-exit(error);
-case 98:
-dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", s);
-exit(error);
-case 99:
-dprintf(STDERR_FILENO, "Error: Can't write to %s\n", s);
-exit(error);
-case 100:
-dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
-exit(error);
-default:
-return (0);
-}
+	char *buffer;
+
+	buffer = malloc(sizeof(char) * 1024);
+
+	if (buffer == NULL)
+	{
+		dprintf(STDERR_FILENO,
+				"Error: Can't write to %s\n", file);
+		exit(99);
+	}
+
+	return (buffer);
 }
 
 /**
- * main - create a copy of file
- *
- * @argc: argument counter
- * @argv: argument vector
- *
- * Return: 0 for success.
-*/
+ * close_file - close the file
+ * @num: int
+ */
+
+void close_file(int num)
+{
+	int c;
+
+	c = close(num);
+
+	if (c == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", num);
+		exit(100);
+	}
+}
+
+/**
+ * main - main function
+ * @argc: int
+ * @argv: parameters
+ * Return: int
+ */
 int main(int argc, char *argv[])
 {
-int file_in, file_out;
-int read_stat, write_stat;
-int close_in, close_out;
-char buffer[MAXSIZE];
+	int from, to, r, w;
+	char *buffer;
 
-/*if arguments are not 3*/
-if (argc != 3)
-__exit(97, NULL, 0);
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
 
-/*sets file descriptor*/
-file_in = open(argv[1], O_RDONLY);
-if (file_in == -1)
-__exit(98, argv[1], 0);
+	buffer = create_str(argv[2]);
+	from = open(argv[1], O_RDONLY);
+	r = read(from, buffer, 1024);
+	to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 
-/*sets file descriptor for copy to file*/
-file_out = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, 0664);
-if (file_out == -1)
-__exit(99, argv[2], 0);
+	do {
+		if (from == -1 || r == -1)
+		{
+			dprintf(STDERR_FILENO,
+					"Error: Can't read from file %s\n", argv[1]);
+			free(buffer);
+			exit(98);
+		}
 
-/*reads file not NULLL*/
-while ((read_stat = read(file_in, buffer, MAXSIZE)) != 0)
-{
-if (read_stat == -1)
-__exit(98, argv[1], 0);
+		w = write(to, buffer, r);
+		if (to == -1 || w == -1)
+		{
+			dprintf(STDERR_FILENO,
+					"Error: Can't write to %s\n", argv[2]);
+			free(buffer);
+			exit(99);
+		}
 
-/*copy and write */
-write_stat = write(file_out, buffer, read_stat);
-if (write_stat == -1)
-__exit(99, argv[2], 0);
-}
+		r = read(from, buffer, 1024);
+		to = open(argv[2], O_WRONLY | O_APPEND);
 
-close_in = close(file_in);
-if (close_in == -1)
-__exit(100, NULL, file_in);
+	} while (r > 0);
 
-close_out = close(file_out);
-if (close_out == -1)
-__exit(100, NULL, file_out);
+	free(buffer);
+	close_file(from);
+	close_file(to);
 
-return (0);
+	return (0);
 }
